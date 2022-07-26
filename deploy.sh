5f3ddd3e4a70779122c683da76a8bbb577efce23
Sakyuba Noe <398noe@gmail.com>
2022-07-26 15:20:52 +0900

    Add build and reboot command

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,11 @@
 #!/bin/bash -eux
 pwd
+
+# build and run
+echo "Frontend : build and run"
+# frontend
+docker build -f ./docker/frontend/Dockerfile ./frontend
+docker ps -a -q -f name=nift-frontend | xargs docker rm -f
+docker run -d --name nift-frontend --restart=always -p 3000:3000 nift-frontend
+
+# backend
