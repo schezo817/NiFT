099410b220b3aa74a2ef58640870d9723302c291
DahuiSheng <sheng.131216@gmail.com>
2022-08-22 16:38:24 +0900

    delete chakras

--- nift/pages/_app.tsx
+++ nift/pages/_app.tsx
@@ -1,11 +1,9 @@
-import { ChakraProvider } from "@chakra-ui/react"
+
 import { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return(
-    <ChakraProvider>
       <Component {...pageProps} />
-    </ChakraProvider>
   );
 };
 
--- nift/pages/index.tsx
+++ nift/pages/index.tsx
@@ -1,12 +1,8 @@
 import { NextPage } from "next";
-import { BaseLayout, Footer } from "../src/index";
 
 const Home: NextPage = () => {
   return (
     <>
-      <BaseLayout>
-        <Footer />
-      </BaseLayout>
     </>
   )
 }
