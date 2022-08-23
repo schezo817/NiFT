7d49a8e36443bc0ab469ef77e9e9173820f5229c
DahuiSheng <sheng.131216@gmail.com>
2022-08-23 12:05:27 +0900

    add top

--- nift/pages/index.tsx
+++ nift/pages/index.tsx
@@ -2,11 +2,13 @@ import { LockClosedIcon } from '@heroicons/react/solid'
 import { NextPage } from "next";
 import Create from './Create ';
 import Login from './login';
+import Top from './top';
 
 const Home: NextPage = () => {
   return (
     <>
       <div>
+        <Top />
         <Login />
       </div>
       <div>
