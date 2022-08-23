2f75213b0b694295a5cfd03764ffed42434ddf0a
DahuiSheng <sheng.131216@gmail.com>
2022-08-23 12:30:40 +0900

    add cardfooter

--- nift/pages/Create .tsx
+++ nift/pages/Create .tsx
@@ -120,6 +120,35 @@ export default function Create() {
                       </div>
                     </div>
                   </div>
+                  <label htmlFor="price" className="block text-sm font-medium text-gray-700">
+                    Price
+                  </label>
+                  <div className="mt-1 relative rounded-md shadow-sm">
+                    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                      <span className="text-gray-500 sm:text-sm">$</span>
+                    </div>
+                    <input
+                      type="text"
+                      name="price"
+                      id="price"
+                      className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+                      placeholder="0.00"
+                    />
+                    <div className="absolute inset-y-0 right-0 flex items-center">
+                      <label htmlFor="currency" className="sr-only">
+                        Currency
+                      </label>
+                      <select
+                        id="currency"
+                        name="currency"
+                        className="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md"
+                      >
+                        <option>USD</option>
+                        <option>CAD</option>
+                        <option>EUR</option>
+                      </select>
+                    </div>
+                  </div>
                   <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
                     <button
                       type="submit"
@@ -139,6 +168,8 @@ export default function Create() {
             <div className="border-t border-gray-200" />
           </div>
         </div>
+
+        
   
         {/* <div className="mt-10 sm:mt-0">
           <div className="md:grid md:grid-cols-3 md:gap-6">
@@ -280,6 +311,8 @@ export default function Create() {
             <div className="border-t border-gray-200" />
           </div>
         </div>
+
+        
   
         {/* <div className="mt-10 sm:mt-0">
           <div className="md:grid md:grid-cols-3 md:gap-6">
--- nift/pages/index.tsx
+++ nift/pages/index.tsx
@@ -1,20 +1,28 @@
 import { LockClosedIcon } from '@heroicons/react/solid'
 import { NextPage } from "next";
 import Create from './Create ';
+import SelectAvatar from './SelectAvatar';
+import Cardfooter from './cardfooter';
+import Dashboard from './dashboard';
 import Login from './login';
 import Navbar from './navbar';
 import Top from './top';
+import Withclose from './withclose';
 
 const Home: NextPage = () => {
   return (
     <>
       <div>
+        <Withclose />
         <Top />
         <Login />
       </div>
       <div>
         <Navbar />
         <Create />
+        <Cardfooter />
+        <Dashboard />
+        <SelectAvatar />
       </div>
     </>
   )
