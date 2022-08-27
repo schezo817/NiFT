441c41ad536329c9a4a7cc324802ae6d080cba67
DahuiSheng <sheng.131216@gmail.com>
2022-08-27 11:49:50 +0900

    UI使用イメージ

--- nift/pages/index.tsx
+++ nift/pages/index.tsx
@@ -3,22 +3,36 @@ import { NextPage } from "next";
 import Create from './Create ';
 import SelectAvatar from './SelectAvatar';
 import Cardfooter from './cardfooter';
+import Centergrid from './centergrid';
 import Dashboard from './dashboard';
+import Expandablesidebar from './expandablesidebar';
+import Justified from './justified';
 import Login from './login';
+import Market from './market';
 import Navbar from './navbar';
+import Quickview from './quickview';
+import Sideover from './sideover';
 import Top from './top';
 import Withclose from './withclose';
+import Withimagegrid from './withimagegrid';
 
 const Home: NextPage = () => {
   return (
     <>
       <div>
-        <Withclose />
+        {/* <Withclose /> */}
         <Top />
         <Login />
+        <Justified />
+        <Sideover />
+        <Centergrid />
+        <Expandablesidebar />
+        <Market />
       </div>
       <div>
         <Navbar />
+        <Quickview />
+        <Withimagegrid />
         <Create />
         <Cardfooter />
         <Dashboard />
