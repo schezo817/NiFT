c8d54a422213325bcbdf1828b4f36e487ccbb26e
DahuiSheng <sheng.131216@gmail.com>
2022-08-28 18:08:52 +0900

    change tailwindcss.js typography

--- nift/tailwind.config.js
+++ nift/tailwind.config.js
@@ -1,15 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-const plugin = require('tailwindcss/plugin')
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        raleway: ['Raleway'],
+        kosugimaru: ['Kosugi Maru'],
+        azeretmono: ['Azeret Mono'],
+      }
+    },
   },
   plugins: [require("daisyui"),
             require('@tailwindcss/forms'),
+            require("@tailwindcss/typography"),
           ],
   
 }
