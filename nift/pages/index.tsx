a6e75ef8393fb14c28ff0ca3a9645a742ea328c6
DahuiSheng <sheng.131216@gmail.com>
2022-08-28 18:49:43 +0900

    pagesとcomponentの整理

--- nift/pages/index.tsx
+++ nift/pages/index.tsx
@@ -1,15 +1,28 @@
+import About from "components/About";
+import Footer from "components/Footer";
+import History from "components/History";
+import Navbar from "components/Navbar";
+import Welcome from "components/Welcome";
+import Works from "components/Works";
+import Meta from "components/_common";
+import { NextPage } from "next";
 import Head from "next/head";
 
-function HomePage() {
+const Home: NextPage = () => {
   return  (
-    <>
-      <Head>
-        <title>NiFT : a gift with NFT</title>
-        <meta name = "description" content = "This is description" />
-      </Head>
+    <div>
+      <Meta pageTitle="a gift with NFT" pageUrl="" pageOgImage={"https://www.nakaatsu.com/images/og_image.png"}/>
       <h1>Hello World</h1>
-    </>
+      <div className = 'bg-gradient-to-br from-lime-600 via-teal-100 to-cyan-300'>
+        <Navbar />
+        <Welcome />
+        <About />
+        <Works />
+        <History />
+        <Footer />
+      </div>
+    </div>
   );
 }
 
-export default HomePage;
+export default Home;
