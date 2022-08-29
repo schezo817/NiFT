2f6fa82c1726670efd18324990b03993d19391de
Sakyuba Noe <398noe@gmail.com>
2022-08-30 01:45:20 +0900

    Fix Footer

--- nift/components/Footer.tsx
+++ nift/components/Footer.tsx
@@ -2,12 +2,15 @@ import Link from "next/link";
 
 const Footer = () => {
     return (
-        <div id="Footer" className="bg-slate-800 py-8 px-6 flex justify-center">
-            <div className="font-raleway text-sm">
-                <Link href="/" className="link text-white">
-                    <span className="link text-white">nift.tk</span>
-                </Link>
+        <div className="fixed bottom-0 w-full">
+            <div id="Footer" className="bg-slate-800 py-8 px-6 flex justify-center">
+                <div className="font-raleway text-sm">
+                    <Link href="/" className="link text-white">
+                        <span className="link text-white">nift.tk</span>
+                    </Link>
+                </div>
             </div>
+
         </div>
     );
 };
