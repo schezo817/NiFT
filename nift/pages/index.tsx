ec39aa0de45a9e554bcda72a564304f5acd2cf71
DahuiSheng <sheng.131216@gmail.com>
2022-08-29 23:56:48 +0900

    Navbar

--- nift/pages/index.tsx
+++ nift/pages/index.tsx
@@ -1,9 +1,31 @@
 import { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
     return (
-        <div className="bg-gradient-to-br from-lime-600 via-teal-100 to-cyan-300">
-
+        <div className="bg-gradient-to-br">
+            <Link href={"/login"}>
+                Login
+            </Link>
+            <div>
+                <textarea className="textarea textarea-primary" placeholder="Bio"></textarea>
+            </div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+            <div className="form-control">
+                <label className="label cursor-pointer">
+                    <span className="label-text">Remember me</span> 
+                    <input type="checkbox" checked="checked" className="checkbox" />
+                </label>
+            </div>
         </div>
     );
 };
