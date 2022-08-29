1341f7d1f9c8baec00f8af5961e1af76f58a8be1
DahuiSheng <sheng.131216@gmail.com>
2022-08-30 08:22:58 +0900

    Create login

--- nift/components/Navbar.tsx
+++ nift/components/Navbar.tsx
@@ -151,15 +151,21 @@ const Navbar = () => {
                         <Link href={"/AboutUS"}>
                             <li>
                                 <a>
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                                     AboutUS
                                 </a>
                             </li>
                         </Link>
                     </ul>
+                    <div className="btn-group">
                     <Link href = {"/login"}>
-                        <a className="btn">Log IN</a>
+                        <button className="btn">Log IN</button>
                     </Link>
+                    <Link href = {"/get"}>
+                        <button className="btn">Get NiFTs</button>
+                    </Link>
+                    </div>
+                    {/* <div className="divider divider-horizontal"></div> */}
                 </div>
         </div>
     );
