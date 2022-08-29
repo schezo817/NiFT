02d65a2303284c15fdd6044c91b4ab7f61df7294
Sakyuba Noe <398noe@gmail.com>
2022-08-29 19:41:04 +0900

    Fix with linter

--- nift/pages/index.tsx
+++ nift/pages/index.tsx
@@ -1,11 +1,7 @@
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
-    return (
-        <div className="bg-gradient-to-br from-lime-600 via-teal-100 to-cyan-300">
-
-        </div>
-    );
+    return <div className="bg-gradient-to-br from-lime-600 via-teal-100 to-cyan-300"></div>;
 };
 
 export default Home;
