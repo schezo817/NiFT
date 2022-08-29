e6907cc1d198c7e6f4dc1a20c7d90e811a6128b6
Sakyuba Noe <398noe@gmail.com>
2022-08-29 18:44:35 +0900

    Delete Unused Components from index.tsx

--- nift/pages/index.tsx
+++ nift/pages/index.tsx
@@ -1,17 +1,9 @@
-import About from "components/About";
-import History from "components/History";
-import Welcome from "components/Welcome";
-import Works from "components/Works";
-
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
     return (
         <div className="bg-gradient-to-br from-lime-600 via-teal-100 to-cyan-300">
-            <Welcome />
-            <About />
-            <Works />
-            <History />
+
         </div>
     );
 };
