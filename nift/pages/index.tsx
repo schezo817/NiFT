0b4ad19203961caa2ee76442225fa21a33de285e
Sakyuba Noe <398noe@gmail.com>
2022-08-31 17:37:16 +0900

    changed tag Image to img because it's out domain src

--- nift/pages/index.tsx
+++ nift/pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <Image src={"https://placeimg.com/260/400/arch"} className="max-w-sm rounded-lg shadow-2xl"/>
+        <img src={"https://placeimg.com/260/400/arch"} className="max-w-sm rounded-lg shadow-2xl"/>
         <div>
           <h1 className="text-5xl font-bold">NiFT | a gift with NFT</h1>
           <p className="py-6"> Buy and sell NFT gifts.</p>
