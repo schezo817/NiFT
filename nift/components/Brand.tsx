aba252f8ec2421325c070dcff9ee8b8ca46101f6
funabiki rikuto <rikuto27gifu@gmail.com>
2022-09-06 13:51:32 +0900

    delete auth file

--- nift/components/Brand.tsx
+++ nift/components/Brand.tsx
@@ -3,11 +3,15 @@ import Link from "next/link";
 const Brand = () => {
     return (
         <div className="avatar">
-        <div className="w-24 rounded-full">
+        {/* <div className="w-24 rounded-full">
             <img src="https://placeimg.com/192/192/people" />
+        </div> */}
+        <div className="carousel w-full">
+        <div className="carousel-item w-full">
+            <img src="https://placeimg.com/800/200/arch" className="w-full" />
         </div>
         </div>
-
+        </div>
     );
 };
 
