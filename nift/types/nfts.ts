777fefabc9d759a8824f85b7ec8ffb82dd0a50d1
Sakyuba Noe <398noe@gmail.com>
2022-09-07 05:55:44 +0900

    Add initial types from DB schema

---
+++ nift/types/nfts.ts
@@ -0,0 +1,39 @@
+export interface reserved_nfts {
+    user_id: string;
+    serie_id: string;
+}
+
+export interface series_nfts {
+    series_id: string;
+    nft_id: string;
+}
+
+export interface series_image {
+    series_id: string;
+    common_image_url: string;
+    rare_image_url: string;
+    veryrare_image_url: string;
+}
+
+export interface series_lot {
+    series_id: string;
+    total_lots: number;
+    total_lots_used: number;
+    rare_lots: number;
+    rare_lots_used: number;
+    veryrare_lots: number;
+    veryrare_lots_used: number;
+}
+
+export interface series_detail {
+    series_id: string;
+    brand_name: string;
+    description: string;
+    royality_first: number;
+}
+
+export interface nft_status {
+    nft_id: string;
+    nft_code: number;
+    is_used: boolean;
+}
