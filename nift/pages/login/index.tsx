c3ace6cc2fa5ec5a0161d9e5e2bb936e9eb70130
DahuiSheng <71542476+DahuiSheng@users.noreply.github.com>
2022-09-06 11:49:05 +0900

    set glass

--- nift/pages/login/index.tsx
+++ nift/pages/login/index.tsx
@@ -3,23 +3,22 @@ import { NextPage } from "next";
 const Login: NextPage = () => {
     return (
         // Heroの作成
-        <div className="hero min-h-screen bg-base-0">
+        <div className="hero min-h-screen bg-gradient-to-r from-indigo-800 to-pink-500">
             {/* Heroのコンテンツの説明 */}
             <div className="hero-content flex-col lg:flex-row-reverse">
                 {/* Heroの中では、テキストを中央揃えする。 */}
                 <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Start and get NiFT!</h1>
-                    <p className="py-10">Start NiFT | NFT + gift</p>
+                    <h1 className="text-5xl font-bold text-white">Start and get NiFT!</h1>
                 </div>
 
                 {/* logincardの実装 */}
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card glass flex-shrink-0 w-full max-w-sm shadow-2xl">
                     {/* １つのセットには、<label>と<input>が要素として入る。 */}
                     <div className="card-body">
                         {/* Email */}
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Email</span>
+                                <span className="label-text text-white">Email</span>
                             </label>
                             <input
                                 type="text"
@@ -31,7 +30,7 @@ const Login: NextPage = () => {
                         {/* Password */}
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Password</span>
+                                <span className="label-text text-white">Password</span>
                             </label>
                             <input
                                 type="text"
@@ -39,7 +38,7 @@ const Login: NextPage = () => {
                                 className="input input-bordered"
                             />
                             <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">
+                                <a href="#" className="label-text-alt link link-hover text-white">
                                     Forgot password?
                                 </a>
                             </label>
@@ -49,7 +48,7 @@ const Login: NextPage = () => {
                         <div className="form-control mt-6 justify-items-center">
                             <button className="btn btn-primary">Log in</button>
                             <div className="text-center ">
-                                <span className="label-text">
+                                <span className="label-text text-white">
                                     let&apos;s create Account and get NFT!
                                 </span>
                             </div>
