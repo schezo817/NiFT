bdb56b272d6f71116022dde30ec5547eedb6b2a1
DahuiSheng <71542476+DahuiSheng@users.noreply.github.com>
2022-09-06 14:46:56 +0900

    w-screen

--- nift/components/Brand.tsx
+++ nift/components/Brand.tsx
@@ -2,15 +2,8 @@ import Link from "next/link";
 
 const Brand = () => {
     return (
-        <div className="avatar">
-        {/* <div className="w-24 rounded-full">
-            <img src="https://placeimg.com/192/192/people" />
-        </div> */}
-        <div className="carousel w-full">
-        <div className="carousel-item w-full">
-            <img src="https://placeimg.com/800/200/arch" className="w-full" />
-        </div>
-        </div>
+        <div className="min-w-fit">
+            <img src="https://placeimg.com/800/200/arch" className="w-screen object-cover" />
         </div>
     );
 };
