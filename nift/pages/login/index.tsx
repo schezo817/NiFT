f8ea28c715822f05ce5072f3cf50abce00b08cc5
Sakyuba Noe <398noe@gmail.com>
2022-09-07 04:06:31 +0900

    Delete comments and forms because we use google oauth only

--- nift/pages/login/index.tsx
+++ nift/pages/login/index.tsx
@@ -3,52 +3,18 @@ import { NextPage } from "next";
 const Login: NextPage = () => {
     return (
         // Heroの作成
-        <div className="hero min-h-screen bg-base-0">
-            {/* Heroのコンテンツの説明 */}
+        <div className="hero min-h-screen bg-base-100">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                {/* Heroの中では、テキストを中央揃えする。 */}
-                <div className="text-center lg:text-left">
+                <div className="text-center lg:text-left p-4">
                     <h1 className="text-5xl font-bold">Start and get NiFT!</h1>
                     <p className="py-10">Start NiFT | NFT + gift</p>
                 </div>
 
-                {/* logincardの実装 */}
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    {/* １つのセットには、<label>と<input>が要素として入る。 */}
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-xl bg-base-100 p-4">
                     <div className="card-body">
-                        {/* Email */}
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input
-                                type="text"
-                                placeholder="email"
-                                className="input input-bordered"
-                            />
-                        </div>
-
-                        {/* Password */}
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input
-                                type="text"
-                                placeholder="password"
-                                className="input input-bordered"
-                            />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">
-                                    Forgot password?
-                                </a>
-                            </label>
-                        </div>
-
-                        {/* buttonの実装 */}
                         <div className="form-control mt-6 justify-items-center">
                             <button className="btn btn-primary shadow-lg shadow-cyan-500/50">Log in</button>
-                            <div className="text-center ">
+                            <div className="text-center">
                                 <span className="label-text">
                                     let&apos;s create Account and get NFT!
                                 </span>
