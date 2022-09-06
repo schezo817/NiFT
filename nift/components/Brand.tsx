26b69327913f055157cf75be9b72e87c1dbbf2d1
funabiki rikuto <rikuto27gifu@gmail.com>
2022-09-06 18:13:35 +0900

    setting brand

--- nift/components/Brand.tsx
+++ nift/components/Brand.tsx
@@ -1,32 +1,52 @@
 import Link from "next/link";
+import NFTCard from "./NFTCard";
 
 const Brand = () => {
     return (
         <div className="bg-white">
-            <div className="min-w-fit">
+            {/* Top画像 */}
+            <div className="min-w-fit min-h-fit">
                 <img src="https://placeimg.com/800/200/arch" className="w-screen object-cover" />
             </div>
+            {/* icon画像 */}
+            <div className="w-1/12 -translate-y-12 translate-x-12 shadow-lg">
+                <img src="https://placeimg.com/192/192/people" alt="icon" className=" rounded-lg"/>
+            </div>
 
-            <div className="mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pt-16 lg:pb-24">
-                <div className="mt-4 lg:row-span-3 lg:mt-0">
+            {/* ブランド名 */}
+            <div className="mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:max-w-7xl lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pt-16 lg:pb-24">
                 <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-                    <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">Basic Tee 6-Pack</h1>
-                </div>
-                    <div className="mt-1">
-                        <h1 className="text-2xl tracking-tight text-gray-900 sm:text-xl">Leonardo da Vinci</h1>
-                    </div>
-                <div className="mt-5">
-                    <p className="text-3xl tracking-tight text-gray-900">$192</p>
+                    <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">Leonardo da Vinci</h1>
                 </div>
 
+                {/* ウォレットID */}
+                <div className="mt-6 input-group">
+                    <select className="select select-bordered">
+                    <option disabled selected>ウォレットIDを選択</option>
+                    <option>artifacts.eth</option>
+                    <option>0x623FC4F577926c0aADAEf11a243754C546C1F98c</option>
+                    </select>
+                    <button className="btn">copy</button>
+                </div>
 
-        
+                {/* ブランド詳細 */}
                 <form className="mt-10">
                     <div className="space-y-6">
                     <p className="text-base text-gray-900">The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: &quot;Black&quot;. Need to add an extra pop of color to your outfit? Our white tee has you covered.</p>
                     </div>
                 </form>
                 </div>
+
+                {/* 保持している保持していたNFTを表示*/}
+                <div className="mt-6 grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 ">
+                <NFTCard />
+                <NFTCard />
+                <NFTCard />
+                <NFTCard />
+                <NFTCard />
+                <NFTCard />
+                <NFTCard />
+                <NFTCard />
             </div>
         </div>
     );
