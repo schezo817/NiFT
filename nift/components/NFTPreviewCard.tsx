60b9699e87ab80d8b4885a97080d449e45ba216a
Sakyuba Noe <398noe@gmail.com>
2022-09-07 13:33:35 +0900

    Preview Card with check images

--- nift/components/NFTPreviewCard.tsx
+++ nift/components/NFTPreviewCard.tsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
 import { NFTPreview } from "types/nfts";
+import { checkIfImages } from "utils/checkIfImages";
 
 type NFTPreviewProps = NFTPreview;
 
 const NFTPreviewCard = (props: NFTPreviewProps) => {
+    const [imageUrl, setImageUrl] = useState("https://placehold.jp/570df8/ffffff/400x400.png?text=400x400");
+
+    useEffect(() => {
+        const exec = async () => {
+            const img = new Image();
+            img.src = props.image_url;
+            img.onload = () => {
+                setImageUrl(props.image_url);
+            }
+            img.onerror = () => {
+                setImageUrl("https://placehold.jp/570df8/ffffff/400x400.png?text=400x400");
+            }
+        }
+        exec();
+    }, [props.image_url]);
+
     return (
         <div className="bg-white">
             <div className="mx-auto max-w-2xl py-8 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
@@ -10,7 +28,7 @@ const NFTPreviewCard = (props: NFTPreviewProps) => {
                     <div className="group relative">
                         <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
                             <img
-                                src={props.image_url}
+                                src={imageUrl}
                                 alt={props.nft_name}
                                 className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                             />
--- nift/utils/checkIfImages.ts
+++ nift/utils/checkIfImages.ts
@@ -1,4 +1,4 @@
-export const checkIfImages = async (url: string) => {
+export const checkIfImages = (url: string) => {
     return new Promise((resolve, reject) => {
         const img = new Image();
         img.src = url;
