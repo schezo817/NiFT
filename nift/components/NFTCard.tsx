7190672655f63b326f326f138d008dd28c763a0c
Sakyuba Noe <398noe@gmail.com>
2022-09-07 12:43:40 +0900

    Changed to components with props value

--- nift/components/NFTCard.tsx
+++ nift/components/NFTCard.tsx
@@ -1,16 +1,19 @@
 import Link from "next/link";
+import { NFT } from "types/nfts";
 
-const NFTCard = () => {
+type NFTProps = NFT;
+
+const NFTCard = (props: NFTProps) => {
     return (
         <div className="bg-white">
             <div className="mx-auto max-w-2xl py-8 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
-                <Link href={"/marketplace/detail"}>
+                <Link href={"marketplace/detail/" + props.nft_id}>
                     <div>
                         <div className="group relative">
                             <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
                                 <img
-                                    src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
-                                    alt="Front of men&#039;s Basic Tee in black."
+                                    src={props.image_url}
+                                    alt={props.nft_name}
                                     className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                                 />
                             </div>
@@ -18,16 +21,16 @@ const NFTCard = () => {
                             <div className="mt-4 flex justify-between">
                                 <div>
                                     <h3 className="text-sm text-gray-700">
-                                        <a href="#">
-                                            <span aria-hidden="true" className="absolute inset-0">
-                                                商品説明
-                                            </span>
-                                            ベーシックTシャツ
-                                        </a>
+                                        <span aria-hidden="true" className="absolute inset-0">
+                                            {props.description}
+                                        </span>
+                                        <span>
+                                            {props.nft_name}
+                                        </span>
                                     </h3>
-                                    <p className="mt-1 text-sm text-gray-500">Black</p>
+                                    <p className="mt-1 text-sm text-gray-500">{props.brand_name}</p>
                                 </div>
-                                <p className="text-sm font-medium text-gray-900">$35</p>
+                                <p className="text-sm font-medium text-gray-900">ETH {props.price}</p>
                             </div>
                         </div>
                     </div>
