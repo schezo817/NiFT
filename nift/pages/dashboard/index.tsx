a034a32eb3b70c15d1bbea63d55cd00f2a49b109
Sakyuba Noe <398noe@gmail.com>
2022-09-08 18:11:55 +0900

    Add user information on dashboard

--- nift/pages/dashboard/index.tsx
+++ nift/pages/dashboard/index.tsx
@@ -1,7 +1,8 @@
 import { NextPage } from "next";
-import Board from "components/Board";
-import NFTMarketCard from "components/NFTMarketCard";
+import Link from "next/link";
 import { useState } from "react";
+import { BsPatchCheck } from "react-icons/bs";
+import NFTMarketCard from "components/NFTMarketCard";
 import { nftItemsTest } from "toy/nftItemsTest";
 import { MarketNFT } from "types/nfts";
 
@@ -13,29 +14,53 @@ const Dashboard: NextPage = () => {
         <div className="bg-white">
             {/* Top画像 */}
             <div className="min-w-fit max-h-32">
-                <img src="https://placeimg.com/800/200/arch" className="w-screen object-cover" />
+                <img
+                    src="https://placeimg.com/1920/800/arch"
+                    className="w-screen max-h-96 object-cover"
+                />
             </div>
             {/* icon画像 */}
-            <div className="min-w-[20%] w-1/12 -translate-y-12 translate-x-12 shadow-lg">
-                <img src="https://placeimg.com/192/192/people" alt="icon" className="rounded-lg" />
+            <div className="min-w-[20%] w-10/12 justify-center -translate-y-12 translate-x-4 md:translate-x-12 md:w-2/3 bg-white rounded-xl">
+                <div className="p-4">
+                    <div className="flex flex-row flex-wrap justify-start gap-4 p-4 w-full">
+                        <div className="avatar px-0 md:px-8">
+                            <div className="w-48 h-48 rounded-full">
+                                <img src="https://placeimg.com/192/192/people" />
+                            </div>
+                        </div>
+                        <div className="flex flex-col justify-center gap-4 p-4 w-96 max-w-full break-all">
+                            <p className="text-4xl font-bold">NiFT Official</p>
+                            <div className="flex items-center">
+                                <BsPatchCheck className="text-2xl mr-2" />
+                                <span>Wallet is connected</span>
+                            </div>
+                            <p>0x9edd29d64b3bcd5378b18ee59fc66e90</p>
+                            <Link href={"/dashboard/user"}>
+                                <div className="btn modal-button btn-primary">編集</div>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            {/* 保持している・していたNFTの情報 */}
-            <div className="mt-6 grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4">
-                {marketNFT?.nfts.map((v, i) => {
-                    return (
-                        <NFTMarketCard
-                            nft_id={v.nft_id}
-                            nft_name={v.nft_name}
-                            series_id={v.series_id}
-                            brand_name={v.brand_name}
-                            description={v.description}
-                            image_url={v.image_url}
-                            price={v.price}
-                            wallet_address={v.wallet_address}
-                        />
-                    );
-                })}
+            <div className="w-full bg-white translate-y-0">
+                <h1 className="p-8 pb-4 text-4xl font-bold">My NFT</h1>
+                {/* 保持している・していたNFTの情報 */}
+                <div className="mt-4 grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4">
+                    {marketNFT?.nfts.map((v, i) => {
+                        return (
+                            <NFTMarketCard
+                                nft_id={v.nft_id}
+                                nft_name={v.nft_name}
+                                series_id={v.series_id}
+                                brand_name={v.brand_name}
+                                description={v.description}
+                                image_url={v.image_url}
+                                price={v.price}
+                                wallet_address={v.wallet_address}
+                            />
+                        );
+                    })}
+                </div>
             </div>
         </div>
     );
