ba9f6164321ba2bfd8494ae3cc8252b25ba121ef
Sakyuba Noe <398noe@gmail.com>
2022-09-10 05:00:18 +0900

    Implemented User Get API

--- nift/pages/api/user.ts
+++ nift/pages/api/user.ts
@@ -4,58 +4,74 @@ import { getToken } from "next-auth/jwt";
 import prisma from "lib/prisma";
 
 export const handler: NextApiHandler = async (req, res) => {
+    /**
+     * check jwt token
+     */
     const token = await getToken({ req });
 
     if (token) {
-        console.log("JSON Web Token", JSON.stringify(token, null, 2));
-        const { method } = req;
+        if (req.method === "GET") {
+            try {
+                // idを取得
+                const { id } = req.query;
+                // Please Add Validation for Request
 
-        switch (method) {
-            /**
-             * ユーザ情報を取得するAPI
-             */
-            case "GET":
-                try {
-                    // Please Add Validation for Request
-                    const id = "";
-                } catch (error) {}
-                break;
-            /**
-             * ユーザ情報を更新するAPI
-             */
-            case "POST":
-                break;
+                const user_id: string = id as string;
+
+                if (user_id === "") {
+                    const response: UserGetFailedResponse = {
+                        status: "failed",
+                        data: {
+                            message: "id is required."
+                        }
+                    }
+                    res.status(400).send(response);
+                } else if (user_id !== "") {
+                    const userResponse = await prisma.user.findUnique({
+                        where: {
+                            id: user_id
+                        }
+                    });
+
+                    console.log(userResponse);
+
+                    if (userResponse) {
+                        const response: UserGetSuccessResponse = {
+                            status: "success",
+                            data: userResponse,
+                        };
+                        res.status(200).json(response);
+                    } else {
+                        const response: UserGetFailedResponse = {
+                            status: "failed",
+                            data: {
+                                message: "User was not found.",
+                            },
+                        };
+                        res.status(404).json(response);
+                    }
+                }
+                const response: UserGetFailedResponse = {
+                    status: "error",
+                    data: {
+                        message: "Something is wrong."
+                    }
+                }
+                res.status(500).json(response);
+             } catch (error) {
+                // ValidationError or more error
+                const response: UserGetFailedResponse = {
+                    status: "error",
+                    data: {
+                        message: "Something is wrong."
+                    }
+                }
+                res.status(500).json(response);
+            }
         }
 
-        try {
-            const id: string = "cl7un6nga0010r4mcd4zu0nov";
-            const userResponse = await prisma.user.findUnique({
-                where: {
-                    id,
-                },
-            });
+        if (req.method === "POST") {
 
-            console.log(userResponse);
-
-            if (userResponse) {
-                const response: UserGetSuccessResponse = {
-                    status: "success",
-                    data: userResponse,
-                };
-                res.status(200).json(response);
-            } else {
-                const response: UserGetFailedResponse = {
-                    status: "erorr",
-                    data: {
-                        message: "User was not found.",
-                    },
-                };
-                res.status(404).json(response);
-            }
-        } catch (error) {
-            // UnAuthorizedError
-            // ValidationError
-            res.status(400).json({});
         }
     } else {
         const response: UserGetFailedResponse = {
