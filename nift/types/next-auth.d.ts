4feeedb39a875ce452a5ce8933f2f959a63d8b2d
Sakyuba Noe <398noe@gmail.com>
2022-09-10 06:03:59 +0900

    Add declare module for next-auth

---
+++ nift/types/next-auth.d.ts
@@ -0,0 +1,16 @@
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth"
+import { DefaultJWT, JWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+    interface Session extends DefaultSession {
+        user: User | JWT;
+        expires: Date;
+        id: string;
+        accessToken: string;
+    }
+
+    interface JWT extends DefaultJWT {
+        id: string;
+        accessToken: string;
+    }
+}
